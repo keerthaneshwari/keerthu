dbb23696da44e1e2da9ea855ae0965428d9fd326
keerthaneshwari <keerthana07995@gmail.com>
2016-10-20 11:21:12 +0530

    Create prg1.c

---
+++ prg1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int n;
+ printf("\nEnter the number");
+ scanf("%d",&n);
+ if(n%2==0)
+ { 
+  printf("\nnumber is even:");
+  else
+  printf("\nnumber is odd:");
+  }
+  return 0;
+  getch();
+  }
